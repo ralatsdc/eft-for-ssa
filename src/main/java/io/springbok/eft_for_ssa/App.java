092925bb48808d8b159b9689ac843af6833781c5
Raymond LeClair <raymond.leclair@addgene.org>
2020-04-07 13:54:40 -0400

    Add project skeleton

---
+++ src/main/java/io/springbok/eft_for_ssa/App.java
@@ -0,0 +1,13 @@
+package io.springbok.eft_for_ssa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/io/springbok/eft_for_ssa/AppTest.java
@@ -0,0 +1,20 @@
+package io.springbok.eft_for_ssa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
