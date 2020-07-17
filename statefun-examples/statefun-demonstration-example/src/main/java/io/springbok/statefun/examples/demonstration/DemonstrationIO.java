6eb03433573f36b5b3649f445dc2edef35832783
William Spear <spearw@gmail.com>
2020-07-16 22:56:30 -0400

    docker-compose.yml configuration complete

--- statefun-examples/statefun-demonstration-example/src/main/java/io/springbok/statefun/examples/demonstration/DemonstrationIO.java
+++ statefun-examples/statefun-demonstration-example/src/main/java/io/springbok/statefun/examples/demonstration/DemonstrationIO.java
@@ -33,6 +33,7 @@ public class DemonstrationIO {
 
   public DemonstrationIO(String kafkaAddress) {
     this.kafkaAddress = Objects.requireNonNull(kafkaAddress);
+    System.out.println("******" + this.kafkaAddress);
   }
 
   public IngressSpec<TrackIn> getIngressSpec() {
