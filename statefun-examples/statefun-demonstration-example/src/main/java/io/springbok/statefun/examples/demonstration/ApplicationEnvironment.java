315f3887229517f44dd3f3fff77b1280e365c57f
Raymond LeClair <raymond.leclair@addgene.org>
2020-10-23 11:45:00 -0400

    Reduce repitition

--- statefun-examples/statefun-demonstration-example/src/main/java/io/springbok/statefun/examples/demonstration/ApplicationEnvironment.java
+++ statefun-examples/statefun-demonstration-example/src/main/java/io/springbok/statefun/examples/demonstration/ApplicationEnvironment.java
@@ -1,39 +1,23 @@
-package io.springbok.statefun.examples.utilities;
+package io.springbok.statefun.examples.demonstration;
 
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class SetPaths {
+public class ApplicationEnvironment {
 
-  public static void setPaths() {
-    setPath("OREKIT_PATH");
-    setPath("TLE_PATH");
-    setPath("PROPERTIES_PATH");
-
-    if (System.getenv("OREKIT_PATH") == null) {
-      String orekitPath = rootPath + "eft-for-ssa/orekit-data";
-      System.setProperty("OREKIT_PATH", orekitPath);
-    }
-
-    if (System.getenv("TLE_PATH") == null) {
-      String tlePath = rootPath + "eft-for-ssa/tle-data/globalstar_tles_05_18_2020.txt";
-      System.setProperty("TLE_PATH", tlePath);
-    }
-
-    if (System.getenv("PROPERTIES_PATH") == null) {
-      String propertiesPath =
-              rootPath
-                      + "eft-for-ssa/statefun-examples/statefun-demonstration-example/src/resources/test.properties";
-      System.setProperty("PROPERTIES_PATH", propertiesPath);
-    }
+  public static void setPathProperties() throws Exception {
+    setPathProperty("OREKIT_PATH");
+    setPathProperty("TLE_PATH");
+    setPathProperty("PROPERTIES_PATH");
   }
 
-  public static void setPath(String property) throws Exception {
-    if (System.getenv(property) == null) {
+  public static void setPathProperty(String property) throws Exception {
+    String path = null;
+    if (System.getenv(property) != null) {
+      path = System.getenv(property);
+    } else {
       String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
       rootPath = rootPath.substring(0, rootPath.indexOf("eft-for-ssa"));
-      String path;
       switch (property) {
         case "OREKIT_PATH":
           path = rootPath + "eft-for-ssa/orekit-data";
@@ -45,14 +29,15 @@ public class SetPaths {
 
         case "PROPERTIES_PATH":
           path = rootPath + "eft-for-ssa/statefun-examples/statefun-demonstration-example/src/resources/test.properties";
+          break;
 
         default:
           throw new Exception("Unexpected property");
       }
-      if (!Files.exists(Paths.get(path))) {
-        throw new Exception(String.format("Path %s does not exist", path));
-      }
-      System.setProperty(property, path);
     }
+    if (!Files.exists(Paths.get(path))) {
+      throw new Exception(String.format("Path %s does not exist", path));
+    }
+    System.setProperty(property, path);
   }
 }
