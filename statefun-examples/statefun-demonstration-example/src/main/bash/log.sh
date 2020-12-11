7d168bd160a6ae5973fc83c5fc8f91ea4e08d891
Raymond LeClair <raymond.leclair@addgene.org>
2020-12-11 09:30:20 -0500

    Add updated packaging files

--- statefun-examples/statefun-demonstration-example/src/main/bash/log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-timestamp=`date "+%Y-%m-%dT%H:%M:%S"`
-consumer='nohup docker-compose exec kafka-broker kafka-console-consumer.sh --bootstrap-server localhost:9092 --isolation-level read_committed --topic default'
-
-$($consumer 2>&1 | grep 'Created track for id' > consumer-$timestamp-created-track.log &)
-$($consumer 2>&1 | grep 'Created orbit for id' > consumer-$timestamp-created-orbit.log &)
-$($consumer 2>&1 | grep 'Correlated orbits with ids' > consumer-$timestamp-correlated-orbits.log &)
-$($consumer 2>&1 | grep 'Refined orbits with id' > consumer-$timestamp-refined-orbits.log &)
--- statefun-examples/statefun-demonstration-example/src/main/bash/package.sh
+++ statefun-examples/statefun-demonstration-example/src/main/bash/package.sh
@@ -1,27 +1,34 @@
+#!/usr/bin/env bash
+
 set -e
 
+# Work in statefun-demonstration-example directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DIR="$( dirname "$( dirname "$( dirname $DIR )" )" )"
-VER=1.0.2
-
 pushd $DIR
 
+# Package this version
+VER=1.0.2
 git checkout v$VER
+rm -rf processor-$VER
+mkdir -p processor-$VER/checkpoint-dir
 
-mkdir processor-$VER
-
+# Run Java build
 mvn clean package -DskipTests
 
+# Run Docker build
 docker-compose down
 docker-compose build --no-cache
+docker tag statefun-demonstration-example_master:latest statefun-demonstration-example_master:$VER
+docker tag statefun-demonstration-example_worker:latest statefun-demonstration-example_worker:$VER
 
 echo "Saving master image ..."
-docker save statefun-demonstration-example_master:latest \
+docker save statefun-demonstration-example_master:$VER \
     | gzip > statefun-demonstration-example_master-$VER.tar.gz
 mv statefun-demonstration-example_master-$VER.tar.gz processor-$VER
 
 echo "Saving worker image ..."
-docker save statefun-demonstration-example_worker:latest \
+docker save statefun-demonstration-example_worker:$VER \
     | gzip > statefun-demonstration-example_worker-$VER.tar.gz
 mv statefun-demonstration-example_worker-$VER.tar.gz processor-$VER
 
@@ -35,13 +42,15 @@ docker save wurstmeister/zookeeper:latest \
     | gzip > wurstmeister-zookeeper-latest.tar.gz
 mv wurstmeister-zookeeper-latest.tar.gz processor-$VER
 
-cp docker-compose.yaml processor-$VER
+# Move needed files into archive directory, and archive
+cat docker-compose.yaml.deploy \
+    | sed "s/<VER>/$VER/" > processor-$VER/docker-compose.yaml
+cat load.sh.deploy \
+    | sed "s/<VER>/$VER/" > processor-$VER/load.sh
 cp docker-variables.env processor-$VER
 cp src/resources/docker.properties processor-$VER
 cp conf/flink-conf.yaml processor-$VER
-
 cp -r ../../orekit-data processor-$VER
-
 tar -czvf processor-$VER.tar.gz processor-$VER
 
 popd
