9bcaa8a5b8ebaa3e63fc640a83cd753902e07306
Raymond LeClair <raymond.leclair@addgene.org>
2021-01-12 11:30:36 -0500

    Clean up analysis code

--- statefun-examples/statefun-demonstration-example/src/main/matlab/plot_orbit_count.m
+++ statefun-examples/statefun-demonstration-example/src/main/matlab/plot_orbit_count.m
@@ -1,96 +1,37 @@
-% gst_14 = load('consumer-14s.dat');
-% 
-% gst_dnm_14 = datenum(gst_14(:, 1), gst_14(:, 2), gst_14(:, 3), gst_14(:, 4), gst_14(:, 5), gst_14(:, 6));
-% gst_onm_14 = gst_14(:, 7);
-% 
-% gst_42 = load('consumer-42s.dat');
-% 
-% gst_dnm_42 = datenum(gst_42(:, 1), gst_42(:, 2), gst_42(:, 3), gst_42(:, 4), gst_42(:, 5), gst_42(:, 6));
-% gst_onm_42 = gst_42(:, 7);
-% 
-% leo_2035_03 = load('leo-tle-2020-11-03.dat');
-% 
-% leo_dnm_2035_03 = datenum(leo_2035_03(:, 1), leo_2035_03(:, 2),
-% leo_2035_03(:, 3), leo_2035_03(:, 4), leo_2035_03(:, 5), leo_203%
-% 5_03(:, 6));
-% leo_onm_2035_03 = leo_2035_03(:, 7);
-% 
-% leo_2035_04 = load('leo-tle-2020-11-04.dat');
-% 
-% leo_dnm_2035_04 = datenum(leo_2035_04(:, 1), leo_2035_04(:, 2),
-% leo_2035_04(:, 3), leo_2035_04(:, 4), leo_2035_04(:, 5), leo_203%
-% 5_04(:, 6));
-% leo_onm_2035_04 = leo_2035_04(:, 7);
-% 
-% leo_2035_05 = load('leo-tle-2020-11-05.dat');
-% 
-% leo_dnm_2035_05 = datenum(leo_2035_05(:, 1), leo_2035_05(:, 2),
-% leo_2035_05(:, 3), leo_2035_05(:, 4), leo_2035_05(:, 5), leo_203%
-% 5_05(:, 6));
-% leo_onm_2035_05 = leo_2035_05(:, 7);
-% 
-% leo_2035_06 = load('leo-tle-2020-11-06.dat');
-% leo_dnm_2035_06 = datenum(leo_2035_06(:, 1), leo_2035_06(:, 2), leo_2035_06(:, 3), leo_2035_06(:, 4), leo_2035_06(:, 5), leo_2035_06(:, 6));
-% leo_onm_2035_06 = leo_2035_06(:, 7);
-
-gs_001 = load('consumer-2020-12-29-001.dat');
-gs_002 = load('consumer-2020-12-29-002.dat');
-gs_004 = load('consumer-2020-12-29-004.dat');
-gs_008 = load('consumer-2020-12-29-008.dat');
-
-dn_001 = datenum(gs_001(:, 1), gs_001(:, 2), gs_001(:, 3), gs_001(:, 4), gs_001(:, 5), gs_001(:, 6));
-dn_002 = datenum(gs_002(:, 1), gs_002(:, 2), gs_002(:, 3), gs_002(:, 4), gs_002(:, 5), gs_002(:, 6));
-dn_004 = datenum(gs_004(:, 1), gs_004(:, 2), gs_004(:, 3), gs_004(:, 4), gs_004(:, 5), gs_004(:, 6));
-dn_008 = datenum(gs_008(:, 1), gs_008(:, 2), gs_008(:, 3), gs_008(:, 4), gs_008(:, 5), gs_008(:, 6));
-
-on_001 = datenum(gs_001(:, 7));
-on_002 = datenum(gs_002(:, 7));
-on_004 = datenum(gs_004(:, 7));
-on_008 = datenum(gs_008(:, 7));
-
-figure(1); hold off;
-offset = [0.05, 0.05, 0.05, 0.05];
-gph = [];
-gth = [];
-glh = [];
-gph = [gph, plot(86400 * (dn_001 - dn_001(1)), on_001 / 3)]; hold on;
-gph = [gph, plot(86400 * (dn_002 - dn_002(1)), on_002 / 3)]; hold on;
-gph = [gph, plot(86400 * (dn_004 - dn_004(1)), on_004 / 3)]; hold on;
-gph = [gph, plot(86400 * (dn_008 - dn_008(1)), on_008 / 3)]; hold on;
-limits = axis;
-legend({'1 s', '2 s', '4 s', '8 s'}, 'location', 'northwest');
-axis([0, 120, limits(3), limits(4)]);
-set(gca, 'FontName', 'Arial', 'FontSize', 14, 'FontWeight', 'demi');
-set(gph, 'LineWidth', 1.5);
-set(gth, 'FontName', 'Arial', 'FontSize', 14, 'FontWeight', 'bold', 'HorizontalAlignment', 'left');
-glh = [glh, xlabel('Run Time')];
-glh = [glh, ylabel('Total States per Object')];
-set(glh, 'FontName', 'Arial', 'FontSize', 14, 'FontWeight', 'demi');
-trim_plot(offset);
-print('plot_orbit_count.pdf', '-dpdf');
-
-function trim_plot(off)
+function plot_orbit_count(data_file_name)
+% Plot number of orbits which exist after each event analyzed.
+%
+% Parameters:
+%   data_file_name - File produced by process_log.py
+%
+% Returns:
+%   none
   
-  set(gca, 'units', 'normalized')
-  % [left, bottom, right, top]
-  ins = get(gca, 'TightInset');
-  % off = [0.01, 0.01, 0.05, 0.10];
-  tot = ins + off;
-  % [left, bottom, width, height]
-  pos = [tot(1), tot(2), 1 - tot(1) - tot(3), 1 - tot(2) - tot(4)];
-  set(gca, 'Position', pos);
-  
-  set(gca, 'units', 'centimeters')
-  % [left, bottom, right, top]
-  ins_cm = get(gca, 'TightInset');
-  off_cm([1, 3]) = off([1, 3]) * ins_cm(1) / ins(1);
-  off_cm([2, 4]) = off([2, 4]) * ins_cm(2) / ins(2);
-  tot_cm = ins_cm + off_cm;
-  % [left, bottom, width, height]
-  pos_cm = get(gca, 'Position');
-  set(gcf, 'PaperUnits', 'centimeters');
-  set(gcf, 'PaperSize', [pos_cm(3) + tot_cm(1) + tot_cm(3), pos_cm(4) + tot_cm(2) + tot_cm(4)]);
-  set(gcf, 'PaperPositionMode', 'manual');
-  set(gcf, 'PaperPosition', [0, 0, pos_cm(3) + tot_cm(1) + tot_cm(3), pos_cm(4) + tot_cm(2) + tot_cm(4)]);
-  
-end % trim_plot(off)
+    % Load the data file and assign date and orbit numbers
+    event_data = load(data_file_name);
+    date_number = datenum( ...
+        event_data(:, 1), event_data(:, 2), event_data(:, 3), ...
+        event_data(:, 4), event_data(:, 5), event_data(:, 6) ...
+    );
+    orbit_number = datenum(event_data(:, 7));
+
+    % Plot the orbit number as a function of date number, and print
+    figure(1); hold off;
+    offset = [0.05, 0.05, 0.05, 0.05];
+    gph = [];
+    gph = [gph, plot(86400 * (date_number - date_number(1)), orbit_number / 3)]; hold on;
+    limits = axis;
+    legend({'1 s', '2 s', '4 s', '8 s'}, 'location', 'northwest');
+    axis([0, 120, limits(3), limits(4)]);
+    set(gca, 'FontName', 'Arial', 'FontSize', 14, 'FontWeight', 'demi');
+    set(gph, 'LineWidth', 1.5);
+    gth = [];
+    set(gth, 'FontName', 'Arial', 'FontSize', 14, 'FontWeight', 'bold', 'HorizontalAlignment', 'left');
+    glh = [];
+    glh = [glh, xlabel('Run Time')];
+    glh = [glh, ylabel('Total States per Object')];
+    set(glh, 'FontName', 'Arial', 'FontSize', 14, 'FontWeight', 'demi');
+    trim_plot(offset);
+    print('plot_orbit_count.pdf', '-dpdf');
+
+end % plot_orbit_count()
--- statefun-examples/statefun-demonstration-example/src/main/python/process_log.py
+++ statefun-examples/statefun-demonstration-example/src/main/python/process_log.py
@@ -1,44 +1,48 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
 import re
-import time
+# import time
 
-# inp_fnm = 'consumer-14s-2020-11-02T23:57+00:00.log'
-# inp_fnm = 'consumer-42s-2020-11-03T00:08+00:00.log'
-# inp_fnm = 'leo-tle-2020-11-03.log'
-# inp_fnm = 'leo-tle-2020-11-04.log'
-# inp_fnm = 'leo-tle-2020-11-05.log'
-# inp_fnm = 'leo-tle-2020-11-06.log'
-# inp_fnm = 'consumer-2020-12-29-001.log'
-# inp_fnm = 'consumer-2020-12-29-002.log'
-# inp_fnm = 'consumer-2020-12-29-003.log'
-# inp_fnm = 'consumer-2020-12-29-004.log'
-inp_fnm = 'consumer-2020-12-29-008.log'
-# inp_fnm = 'consumer-2020-12-29-032.log'
-#inp_fnm = 'consumer-2020-12-29-320.log'
+if __name__ == "__main__":
 
-out_fnm = inp_fnm.replace('.log', '.dat')
+    # Add and parse arguments
+    parser = ArgumentParser()
 
-created_ptn = re.compile('Created orbit for id')
-refined_ptn = re.compile('Refined orbits with ids')
-cleared_ptn = re.compile('Cleared orbit for id')
+    parser.add_argument(
+        "-l",
+        "--log-file-name",
+        default="consumer.log",
+        help="consumer log file",
+    )
+    options = parser.parse_args()
 
-num_orb = 0
-with open(out_fnm, 'w') as ofp:
-    with open(inp_fnm, 'r') as ifp:
-        while True:
-            line = ifp.readline()
-            # print(line)
-            if not line:
-                break
-            if created_ptn.search(line) is not None:
-                num_orb += 1
-            if refined_ptn.search(line) is not None:
-                num_orb += 1
-            if cleared_ptn.search(line) is not None:
-                num_orb -= 1
-            flds = line.split()
-            y, m, d = flds[0].replace('[', '').split('-')
-            H, M, S = flds[1].replace(']', '').split(':')
-            msg = "{0} {1} {2} {3} {4} {5:.3f} {6}".format(y, m, d, H, M, float(S), num_orb)
-            print(msg)
-            ofp.write(msg + '\n')
-            # time.sleep(0.001)
+    # Define patterns to identify events required for analysis
+    created_ptn = re.compile('Created orbit for id')
+    refined_ptn = re.compile('Refined orbits with ids')
+    cleared_ptn = re.compile('Cleared orbit for id')
+
+    # Count number of orbits which exist after each event analyzed,
+    # and print along with date and time components for analysis
+    num_orb = 0
+    out_file_name = options.log_file_name.replace('.log', '.dat')
+    with open(out_file_name, 'w') as ofp:
+        with open(options.inp_file_name_, 'r') as ifp:
+            while True:
+                line = ifp.readline()
+                # print(line)
+                if not line:
+                    break
+                if created_ptn.search(line) is not None:
+                    num_orb += 1
+                if refined_ptn.search(line) is not None:
+                    num_orb += 1
+                if cleared_ptn.search(line) is not None:
+                    num_orb -= 1
+                flds = line.split()
+                y, m, d = flds[0].replace('[', '').split('-')
+                H, M, S = flds[1].replace(']', '').split(':')
+                msg = "{0} {1} {2} {3} {4} {5:.3f} {6}".format(y, m, d, H, M, float(S), num_orb)
+                print(msg)
+                ofp.write(msg + '\n')
+                # time.sleep(0.001)
