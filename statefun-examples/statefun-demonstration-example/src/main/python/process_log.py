3eb1915f593436b463a40be0550ac90b5be2cf89
Raymond LeClair <raymond.leclair@addgene.org>
2021-02-12 10:07:09 -0500

    Add requirements and generalize

--- statefun-examples/statefun-demonstration-example/src/main/python/process_log.py
+++ statefun-examples/statefun-demonstration-example/src/main/python/process_log.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 
 from argparse import ArgumentParser
+import matplotlib.pyplot as plt
+import numpy as np
+import os
 import re
 # import time
 
 
-def main():
-
-    # Add and parse arguments
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-l",
-        "--log-file-path",
-        default="consumer.log",
-        help="consumer log file path",
-    )
-    options = parser.parse_args()
+def count_number_of_orbits(options):
 
     # Define patterns to identify events required for analysis
     created_ptn = re.compile('Created orbit for id')
@@ -25,7 +18,9 @@ def main():
     # Count number of orbits which exist after each event analyzed,
     # and print along with date and time components for analysis
     num_orb = 0
-    out_file_path = options.log_file_path.replace('.log', '.dat')
+    num_orb_list = []
+    date_time_list = []
+    out_file_path = options.log_file_path.replace(".log", ".dat")
     with open(out_file_path, 'w') as ofp:
         with open(options.log_file_path, 'r') as ifp:
             while True:
@@ -40,12 +35,62 @@ def main():
                 if cleared_ptn.search(line) is not None:
                     num_orb -= 1
                 flds = line.split()
-                y, m, d = flds[0].replace('[', '').split('-')
-                H, M, S = flds[1].replace(']', '').split(':')
+                date = flds[0].replace("[", "")
+                y, m, d = date.split("-")
+                time = flds[1].replace("]", "")
+                H, M, S = time.split(":")
                 msg = "{0} {1} {2} {3} {4} {5:.3f} {6}".format(y, m, d, H, M, float(S), num_orb)
                 print(msg)
                 ofp.write(msg + '\n')
                 # time.sleep(0.001)
+                num_orb_list.append(num_orb)
+                date_time_list.append(date + "T" + time)
+
+    # Convert date and time to seconds in the run, and numpy arrays
+    date_time = np.array(date_time_list, dtype='datetime64[ms]')
+    seconds = (date_time - date_time[0]).astype('timedelta64[s]')
+    number_of_orbits = np.array(num_orb_list)
+
+    return seconds, number_of_orbits
+
+
+def plot_number_of_orbits(options, seconds, number_of_orbits):
+
+    # Plot number of orbits as a function of run seconds
+    fig, ax = plt.subplots()
+    ax.plot(seconds, number_of_orbits)
+    head, file_name = os.path.split(options.log_file_path)
+    head, file_dir = os.path.split(head)
+    ax.set_title(os.path.join(file_dir, file_name))
+    ax.set_xlabel("Run time [s]")
+    ax.set_ylabel("Number of Orbits")
+    plt_file_path = options.log_file_path.replace(".log", ".png")
+    plt.savefig(plt_file_path)
+    plt.show()
+
+
+def main():
+
+    # Add and parse arguments
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-l",
+        "--log-file-path",
+        default="consumer.log",
+        help="consumer log file path",
+    )
+    parser.add_argument(
+        "-c",
+        "--count-number-of-orbits",
+        action="store_true",
+        help="count and plot number of orbits as a function of run seconds",
+    )
+    options = parser.parse_args()
+
+    # Process arguments
+    if options.count_number_of_orbits:
+        seconds, number_of_orbits = count_number_of_orbits(options)
+        plot_number_of_orbits(options, seconds, number_of_orbits)
 
 
 if __name__ == "__main__":
